79876d3478f6c846c1e58631c523489d0f08b9b8
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-10-11 18:51:51 +0000

    [Add]マイページ編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,5 +4,18 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find_by(id: current_customer)
+  end
+  
+  def update
+    customer = Customer.find_by(id: current_customer)
+    customer.update(customer_params)
+    redirect_to my_page_path
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:company_name, :email, :post_code, :address, :telephone_number, :last_name, :first_name)
   end
 end
