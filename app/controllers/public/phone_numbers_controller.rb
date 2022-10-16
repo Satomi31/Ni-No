28a09a72a622583802c7815fd6b539f52a016ae8
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-10-16 13:55:00 +0000

    [Add]カートに入れるボタン編集

--- app/controllers/public/phone_numbers_controller.rb
+++ app/controllers/public/phone_numbers_controller.rb
@@ -2,6 +2,8 @@ class Public::PhoneNumbersController < ApplicationController
 
   def index
     @phone_numbers = PhoneNumber.where(sale_status: 0)
+    cart = current_customer.cart_items
+    @cart_items = cart.all
     @cart_item = CartItem.new
   end
 
@@ -13,6 +15,8 @@ class Public::PhoneNumbersController < ApplicationController
     @phone_numbers = PhoneNumber.search(params[:keyword]).where(sale_status: 0)
     @word = params[:keyword]
     @cart_item = CartItem.new
+    cart = current_customer.cart_items
+    @cart_items = cart.all
     render :index
   end
 end
