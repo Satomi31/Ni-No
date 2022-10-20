ad2b6fc8bcdb77d5ebab147b8e61b6e6614064fa
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-10-20 17:43:19 +0000

    [Add]会員退会時、ログイン不可

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,28 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
+
+  def after_sign_out_path_for(current_customer)
+    root_path
+  end
+
+  def after_sign_in_path_for(current_customer)
+    root_path
+  end
+
+  protected
+  # 退会しているかを判断するメソッド
+  def customer_state
+  # 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+  # アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+  # 取得したアカウントのパスワードと入力されたパスワードが一致してるか、かつ退会フラグが退会か
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted == true
+      redirect_to new_customer_registration_path
+    end
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,4 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :phone_number
 
-  validates :customer_id, presence: true
-  validates :phone_number_id, presence: true
-
 end
